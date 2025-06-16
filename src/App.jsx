ab200531e1ef5c275ab466cd357bfe2b9644ec97
Viktor Lychkatyi <137647165+ViktorLychkatyi@users.noreply.github.com>
2025-06-16 14:16:54 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         </div>
 
         <div id={"biography"}>
-           <div className={"picture_biography"}><img src="../public/img/Imagine-Dragons.jpg" alt=""/></div>
+           <div className={"picture_biography"}><img src="./img/Imagine-Dragons.jpg" alt=""/></div>
             <h1 className={"title_info"}>БИОГРАФИЯ</h1>
             <p className={"info_width"}>Imagine Dragons — американская рок-группа из Лас-Вегаса, основанная в 2008 году. Мировую славу коллектив приобрёл после выхода сингла "It's Time", а затем альбома "Night Visions" (2012), в который вошёл мегахит "Radioactive". Их музыка — это синтез альтернативного рока, попа и электронной энергии, подкреплённый искренними текстами и мощной подачей.</p>
         </div>
