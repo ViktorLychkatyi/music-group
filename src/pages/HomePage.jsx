41619fb8d7864bfd205f72e21034d66b66ec4458
Viktor Lychkatyi <137647165+ViktorLychkatyi@users.noreply.github.com>
2025-06-23 08:28:22 +0300

    test

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
 import Player from "../components/UI/Layout/Player.jsx";
-import "../index.css";
-import "../App.css";
 import Header from "../components/UI/Layout/Header.jsx";
 import Biography from "../components/UI/Layout/Biography.jsx";
 import Info from "../components/UI/Layout/Info.jsx";
 import Album from "../components/UI/Layout/Album.jsx";
 import Gallery from "../components/UI/Layout/Gallery.jsx";
+import "../index.css";
+import "../App.css";
 
 function HomePage() {
     return (
